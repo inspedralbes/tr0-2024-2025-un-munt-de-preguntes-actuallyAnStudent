c0bde733583ede5c60dde9e5561506c3f4aa4c27
a23benromdor <a23benromdor@inspedralbes.cat>
2024-09-21 00:00:28 +0200

    Es registren les respostes, post navegar entre les preguntes, els botons s'oculten i es mostren depenen de la situacio.

--- web/js/functions.js
+++ web/js/functions.js
@@ -1,35 +1,112 @@
-const URL = "http://localhost/Projecte_Back/data.json";
+let data;
+const estatDeLaPartida = 
+  {
+    contPregunta: 0,
+    preguntes: []
+  };
+const URL = "http://localhost/tr0-2024-2025-un-munt-de-preguntes-actuallyAnStudent/back/data.json";
 fetch(URL)
 .then(response => response.json())
-.then(data => renderPreguntes(data));
+.then(dades => partida(dades));
 
-function renderPreguntes(info){
-  let strElement = '';
+function partida(info){
   data = info.preguntes;
 
-  const tauler = document.getElementById("preguntes");
-  for (let index = 0; index < data.length; index++) {
-    strElement += `<p>${data[index].pregunta}</p><br>`;
-    for (let it = 0; it < data[index].respostes.length; it++) {
-      //strElement += `<button onclick="verify(${data[index].resposta_correcta},${data[index].respostes[it].id})">${data[index].respostes[it].etiqueta}</button>`;
-      strElement += `<button onclick="verify(${index},${it})">${data[index].respostes[it].etiqueta}</button>`;
-      //strElement += `<img src="" alt="${data[index].respostes[it].etiqueta} image"><br>`;
-    }
-  }
-  tauler.innerHTML = strElement;
+  omplirPreguntes();
+  imprimirPregunta();
 }
 
-/*function play(){
-  let estatDeLaPartida = {
-    preguntesFetes: [],
-    respCorrectes: 0, 
+function omplirPreguntes(){
+  for (let index = 0; index < data.length; index++) {
+    let pregunta = {
+      id: index,
+      resposta: -1
+    };
+    estatDeLaPartida.preguntes.push(pregunta);
+  }
+}
+
+function imprimirPregunta(){
+  const tauler = document.getElementById("partida");
+  let strElement = '';
+
+  strElement += `<p>${data[estatDeLaPartida.contPregunta].pregunta}</p><br>`;
+  for (let i = 0; i < data[estatDeLaPartida.contPregunta].respostes.length; i++) {
+    strElement += `<button class="resposta" onclick="buttonPressed(${i})">${data[estatDeLaPartida.contPregunta].respostes[i].etiqueta}</button>`;
+  }
+  strElement += `<button id="cancel" class="reset" onclick="resetResposta(${estatDeLaPartida.contPregunta})">Cancel·lar resposta</button>`;
+  strElement += `<button class="anterior" onclick="lastQuestion()">Anterior</button>`;
+  strElement += `<button class="seguent" onclick="nextQuestion()">Seguent</button>`;
+  strElement += `<button class="enviar" onclick="enviar()">Enviar</button>`;//no he hecho la funcion todavia
+  tauler.innerHTML = strElement;
+
+  const botones = tauler.querySelectorAll("[class]");
+  botones.forEach(boto => {
+    verificacions(boto);
+  }); 
+}
+
+function buttonPressed(idRespSel){
+  alert(`Soc la resposta ${data[estatDeLaPartida.contPregunta].respostes[idRespSel].id} de la pregunta ${data[estatDeLaPartida.contPregunta].pregunta}`);
+  modificarPreguntes(idRespSel);
+  //con el eventListener lo quitare esto de abajo
+  const tauler = document.getElementById("partida");
+  const botones = tauler.querySelectorAll("[class]");
+  botones.forEach(boto => {
+    verificacions(boto);
+  }); 
+}
+
+function modificarPreguntes(idRespSel){
+  let pregunta = {
+    id: estatDeLaPartida.contPregunta,
+    resposta: data[estatDeLaPartida.contPregunta].respostes[idRespSel].id-1
+  };
+  estatDeLaPartida.preguntes.splice(estatDeLaPartida.contPregunta, 1, pregunta);
+}
+
+function resetResposta(){
+  let pregunta = {
+    id: estatDeLaPartida.contPregunta,
+    resposta: -1
   };
 
+  estatDeLaPartida.preguntes.splice(estatDeLaPartida.contPregunta, 1, pregunta);
 
-}*/
+  //con el eventListener lo quitare esto de abajo
+  const tauler = document.getElementById("partida");
+  const botones = tauler.querySelectorAll("[class]");
+  botones.forEach(boto => {
+    verificacions(boto);
+  }); 
+}
 
-function verify(itPregunta,respSelec){
-  alert("funciona");
+function lastQuestion() {
+  estatDeLaPartida.contPregunta--;
+  imprimirPregunta();
+}
+
+function nextQuestion() {
+  estatDeLaPartida.contPregunta++;
+  imprimirPregunta();
+}
+
+function verificacions(boto){
+  if (boto.classList.contains("anterior") && estatDeLaPartida.contPregunta==0) {
+    boto.className = "notUsable";
+  }
+  if (boto.classList.contains("seguent") && estatDeLaPartida.contPregunta==data.length-1) {
+    boto.className = "notUsable";
+  }
+  if (boto.classList.contains("enviar") && estatDeLaPartida.contPregunta!=data.length-1) {
+    boto.className = "notUsable";
+  }
+  if (boto.classList.contains("reset") && estatDeLaPartida.preguntes[estatDeLaPartida.contPregunta].resposta==-1) {
+    boto.className = "notUsable";
+  }
+  if (boto.id === "cancel" && estatDeLaPartida.preguntes[estatDeLaPartida.contPregunta].resposta!=-1) {
+    boto.className = "reset";
+  }
 }
 
 let time=30;
@@ -38,7 +115,7 @@ const interval = setInterval(() => stillPlaying(timer), 1000);
 
 function stillPlaying(place){
   if (time>0) {
-    place.innerHTML=time;//no me lo muestra pero el console si
+    place.innerHTML=time;
     console.log(time);
     time--;
   }else{
@@ -46,14 +123,3 @@ function stillPlaying(place){
     place.innerHTML="time out"
   }
 }
-
-
-/*for (let index = 0; index < data.preguntes.length; index++) {
-  strElement = `<p>${data.preguntes[index].pregunta}</p><br>`;
-  document.write(strElement);
-  let botElement='';
-  for (let it = 0; it < data.preguntes[index].respostes.length; it++) {
-    botElement = `<button onclick="verify(${data.preguntes[index].resposta_correcta},${data.preguntes[index].respostes[it].id})">${data.preguntes[index].respostes[it].etiqueta}</button>`;
-    document.write(botElement);
-  }
-}*/
